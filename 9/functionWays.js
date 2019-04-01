3522f0d98470071ac0a03825aad506d8ee2d0866
Aswinpchn <aswinpchn@gmail.com>
2019-04-02 03:39:15 +0530

    Ways of writing a function.

--- 9/functionWays.js
+++ 9/functionWays.js
@@ -1,9 +1,9 @@
-function addOne(x) {     // Conventional way.
+function addOne(x) {     // Conventional way.  // Function declaration
     return x + 1;
 }
 var six = addOne(5);
 
-var addOne = function(x) {    // 1st alternate way.  // although it reminds us a bit of how object methods are defined[addOne : function() {}]
+var addOne = function(x) {    // 1st alternate way.  // although it reminds us a bit of how object methods are defined[addOne : function() {}]  // Function exptession.
     return x + 1;
 } ;
 var six = addOne(5);  
